2e089dc23c3faeb98e5a662f537e9d1ca3007f7f
mehdi-benhariz <benharizmehdi20@gmail.com>
2022-01-29 11:06:50 +0100

    added theme toggle

---
+++ client/js/PostAPI.js
@@ -0,0 +1,23 @@
+const baseUrl = "http://localhost:8000/news";
+
+const addPost = async (post) => {
+  const response = await fetch(`${baseUrl}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(post),
+  });
+  return response.json();
+};
+
+const removePost = async (post) => {
+  const response = await fetch(`${baseUrl}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(post),
+  });
+  return response.json();
+};
